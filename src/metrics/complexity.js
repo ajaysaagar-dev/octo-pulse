e538695cce1509cc551609ebadcc22386512ed35
ajaysaagar-dev <ajaysaagar.dev@gmail.com>
2026-09-02 05:51:43 +0530

    feat(metrics): add repository directory depth complexity analyzer

---
+++ src/metrics/complexity.js
@@ -0,0 +1,8 @@
+﻿export function analyzeDirectoryDepth(filePaths = []) {
+    let maxDepth = 0;
+    for (const p of filePaths) {
+        const depth = p.split(/[\\/]/).length;
+        if (depth > maxDepth) maxDepth = depth;
+    }
+    return { maxDepth, recommended: maxDepth <= 5 };
+}
