86f90a48dcffc891d0cace774c2aedb4cefef295
ajaysaagar-dev <ajaysaagar.dev@gmail.com>
2026-09-02 05:51:43 +0530

    feat(metrics): add code churn and change frequency calculator

---
+++ src/metrics/churn.js
@@ -0,0 +1,8 @@
+﻿export function calculateCodeChurn(changes = []) {
+    let additions = 0, deletions = 0;
+    for (const c of changes) {
+        additions += c.additions || 0;
+        deletions += c.deletions || 0;
+    }
+    return { totalChanges: additions + deletions, additions, deletions, net: additions - deletions };
+}
