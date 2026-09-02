eb39b8db0713b474e11de53e7fd42ee40ce26b12
ajaysaagar-dev <ajaysaagar.dev@gmail.com>
2026-09-02 05:51:44 +0530

    feat(ui): add ANSI terminal color styling palette

---
+++ src/ui/colors.js
@@ -0,0 +1,7 @@
+﻿export const colors = {
+    cyan: (txt) => `\x1b[36m${txt}\x1b[0m`,
+    green: (txt) => `\x1b[32m${txt}\x1b[0m`,
+    yellow: (txt) => `\x1b[33m${txt}\x1b[0m`,
+    red: (txt) => `\x1b[31m${txt}\x1b[0m`,
+    bold: (txt) => `\x1b[1m${txt}\x1b[0m`
+};
