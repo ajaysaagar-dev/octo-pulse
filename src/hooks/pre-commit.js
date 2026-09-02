622ba0062575eee0c054867d53bc50bb9928f9aa
ajaysaagar-dev <ajaysaagar.dev@gmail.com>
2026-09-02 05:51:43 +0530

    feat(hooks): add Git pre-commit security hook generator

---
+++ src/hooks/pre-commit.js
@@ -0,0 +1,3 @@
+﻿export function generatePreCommitHookScript() {
+    return `#!/bin/sh\n# OctoPulse Pre-commit Security Hook\nnode bin/octo-pulse.js scan\nif [ $? -ne 0 ]; then\n  echo "Commit blocked by OctoPulse security scan."\n  exit 1\nfi\n`;
+}
